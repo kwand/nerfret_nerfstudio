bd9a79d7137f144e6ace9ecbfa49e8112af718ac
GavinGuan95 <guanyushi9593@gmail.com>
2024-02-27 22:21:40 -0500

    adding uint8 stuff again

--- nerfstudio/pipelines/base_pipeline.py
+++ nerfstudio/pipelines/base_pipeline.py
@@ -138,7 +138,7 @@ class Pipeline(nn.Module):
             self.datamanager.train_sampler.set_epoch(step)
         ray_bundle, batch = self.datamanager.next_train(step)
         model_outputs = self.model(ray_bundle, batch)
-        if batch['image'].dtype == torch.uint8: # 
+        if 'image' in batch and batch['image'].dtype == torch.uint8: # 
             batch['image'] = batch['image'].float() / 255.0 # 
         metrics_dict = self.model.get_metrics_dict(model_outputs, batch)
         loss_dict = self.model.get_loss_dict(model_outputs, batch, metrics_dict)
@@ -300,6 +300,8 @@ class VanillaPipeline(Pipeline):
         """
         ray_bundle, batch = self.datamanager.next_train(step)
         model_outputs = self._model(ray_bundle)  # train distributed data parallel model if world_size > 1
+        if 'image' in batch and batch['image'].dtype == torch.uint8: # 
+            batch['image'] = batch['image'].float() / 255.0 # 
         metrics_dict = self.model.get_metrics_dict(model_outputs, batch)
         loss_dict = self.model.get_loss_dict(model_outputs, batch, metrics_dict)
 
